efd7041a42c7e5a2e3132d74cc5229821910f3d3
CHOI MIN JIN <chlalswls2409@naver.com>
2021-05-18 11:05:06 +0900

    OpenNameActivity

--- app/src/main/java/com/smpexample/lotto/MainActivity.kt
+++ app/src/main/java/com/smpexample/lotto/MainActivity.kt
@@ -11,10 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val cardView = findViewById<CardView>(R.id.cardConstell)
+        val cardName = findViewById<CardView>(R.id.cardName)
         var intent : Intent
 
         cardView.setOnClickListener {
             intent = Intent(this@MainActivity, ConstellationActivity::class.java)
+            startActivity(intent)
+        }
+        cardName.setOnClickListener {
+            intent = Intent(this@MainActivity, NameActivity::class.java)
             startActivity(intent)
         }
     }
