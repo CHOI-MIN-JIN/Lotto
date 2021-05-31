46e0886b00c676e0b7c4d8a0b84957163af01478
CHOI MIN JIN <chlalswls2409@naver.com>
2021-06-01 06:37:33 +0900

    Ball Image Update Function

--- app/src/main/java/com/smpexample/lotto/ResultActivity.kt
+++ app/src/main/java/com/smpexample/lotto/ResultActivity.kt
@@ -12,12 +12,15 @@ class ResultActivity : AppCompatActivity() {
 
         val result = intent.getIntegerArrayListExtra("result") ?: return
 
-        val result_sorted = result?.sortedBy{it}
+//        val result_sorted = result?.sortedBy{it}
+        result?.Let{ 
+            updateLottoBallImages(result.sortedBy{it})
+        }
 
         val LottoImageStartId = R.drawable.ball_01
 //        val LottoImageId2 = R.drawable.ball_02
 //        val LottoImageId3 = R.drawable.ball_03
-        val ImageView1 = findViewById<ImageView>(R.id.ImageView1)
+/*        val ImageView1 = findViewById<ImageView>(R.id.ImageView1)
         val ImageView2 = findViewById<ImageView>(R.id.ImageView2)
         val ImageView3 = findViewById<ImageView>(R.id.ImageView3)
         val ImageView4 = findViewById<ImageView>(R.id.ImageView4)
@@ -29,6 +32,29 @@ class ResultActivity : AppCompatActivity() {
         ImageView3.setImageResource(LottoImageStartId + result_sorted[2] - 1)
         ImageView4.setImageResource(LottoImageStartId + result_sorted[3] - 1)
         ImageView5.setImageResource(LottoImageStartId + result_sorted[4] - 1)
+        ImageView6.setImageResource(LottoImageStartId + result_sorted[5] - 1) */
+    }
+    private fun updateLottoBallImages(result_sorted : List<Int>) {
+        val LottoImageStartId = R.drawable.ball_01
+
+        val ImageView1 = findViewById<ImageView>(R.id.ImageView1)
+        val ImageView2 = findViewById<ImageView>(R.id.ImageView2)
+        val ImageView3 = findViewById<ImageView>(R.id.ImageView3)
+        val ImageView4 = findViewById<ImageView>(R.id.ImageView4)
+        val ImageView5 = findViewById<ImageView>(R.id.ImageView5)
+        val ImageView6 = findViewById<ImageView>(R.id.ImageView6)
+
+        ImageView1.setImageResource(LottoImageStartId + result_sorted[0]!! - 1)
+        ImageView2.setImageResource(LottoImageStartId + result_sorted[1] - 1)
+        ImageView3.setImageResource(LottoImageStartId + result_sorted[2] - 1)
+        ImageView4.setImageResource(LottoImageStartId + result_sorted[3] - 1)
+        ImageView5.setImageResource(LottoImageStartId + result_sorted[4] - 1)
         ImageView6.setImageResource(LottoImageStartId + result_sorted[5] - 1)
     }
 }
+
+private fun <E> ArrayList<E>.Let(function: () -> Unit) {
+
+}
+
+
