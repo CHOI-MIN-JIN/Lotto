c78b57612b0ff65b8a42722bc7a82eed6e605fd6
CHOI MIN JIN <chlalswls2409@naver.com>
2021-06-01 06:18:34 +0900

    Display lotto balls

--- app/src/main/java/com/smpexample/lotto/ResultActivity.kt
+++ app/src/main/java/com/smpexample/lotto/ResultActivity.kt
@@ -1,11 +1,34 @@
 package com.smpexample.lotto
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val result = intent.getIntegerArrayListExtra("result") ?: return
+
+        val result_sorted = result?.sortedBy{it}
+
+        val LottoImageStartId = R.drawable.ball_01
+//        val LottoImageId2 = R.drawable.ball_02
+//        val LottoImageId3 = R.drawable.ball_03
+        val ImageView1 = findViewById<ImageView>(R.id.ImageView1)
+        val ImageView2 = findViewById<ImageView>(R.id.ImageView2)
+        val ImageView3 = findViewById<ImageView>(R.id.ImageView3)
+        val ImageView4 = findViewById<ImageView>(R.id.ImageView4)
+        val ImageView5 = findViewById<ImageView>(R.id.ImageView5)
+        val ImageView6 = findViewById<ImageView>(R.id.ImageView6)
+
+        ImageView1.setImageResource(LottoImageStartId + result_sorted[0] - 1)
+        ImageView2.setImageResource(LottoImageStartId + result_sorted[1] - 1)
+        ImageView3.setImageResource(LottoImageStartId + result_sorted[2] - 1)
+        ImageView4.setImageResource(LottoImageStartId + result_sorted[3] - 1)
+        ImageView5.setImageResource(LottoImageStartId + result_sorted[4] - 1)
+        ImageView6.setImageResource(LottoImageStartId + result_sorted[5] - 1)
     }
 }
