32c213d0aa25ef5898b75812ea5e651f0cea886c
CHOI MIN JIN <chlalswls2409@naver.com>
2021-06-01 05:28:56 +0900

    Random Number Generation

--- app/src/main/java/com/smpexample/lotto/MainActivity.kt
+++ app/src/main/java/com/smpexample/lotto/MainActivity.kt
@@ -4,6 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun getRandomLottoNumber () : Int{
+    return  Random().nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int> {
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true) {
+        var number : Int = getRandomLottoNumber()
+        var flag_exist = 0
+
+        if(lottoNumbers.size < 2){
+            lottoNumbers.add(number)
+            continue
+        }
+        else{
+            for(j in 0 until lottoNumbers.size) {
+                if(number == lottoNumbers[j]) {
+                    flag_exist = 1
+                    break
+                }
+            }
+            if (flag_exist == 0)
+                lottoNumbers.add(number)
+            if (lottoNumbers.size >= 6)
+                break
+        }
+
+    }
+    return lottoNumbers
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +50,15 @@ class MainActivity : AppCompatActivity() {
         var intent : Intent
 
         cardRandom.setOnClickListener {
-            intent = Intent(this@MainActivity, ResultActivity::class.java)
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
             startActivity(intent)
         }
         cardConstell.setOnClickListener {
-            intent = Intent(this@MainActivity, ConstellationActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
         }
         cardName.setOnClickListener {
-            intent = Intent(this@MainActivity, NameActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this@MainActivity, NameActivity::class.java))
         }
     }
 }
